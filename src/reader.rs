1d98a871217a49da08f0c6f2e9d02d3bc8647d12
fireflower0 <fireflower0@hotmail.co.jp>
2019-11-05 00:01:07 +0900

    Fixed a reader

--- src/reader.rs
+++ src/reader.rs
@@ -18,3 +18,80 @@ struct Reader {
     tokens: Vec<String>,
     pos: usize,
 }
+
+impl Reader {
+    fn next(&mut self) -> Result<String, RlErr> {
+        self.pos = self.pos + 1;
+        Ok(self
+           .tokens
+           .get(self.pos - 1)
+           .ok_or(ErrString("underflow".to_string()))?
+           .to_string())
+    }
+    fn peek(&self) -> Result<String, RlErr> {
+        Ok(self
+           .tokens
+           .get(self.pos)
+           .ok_or(ErrString("underflow".to_string()))?
+           .to_string())
+    }
+}
+
+fn tokenize(str: &str) -> Vec<String> {
+    lazy_static! {
+        static ref RE: Regex = Regex::new(
+            r###"[\s,]*(~@|[\[\]{}()'`~^@]|"(?:\\.|[^\\"])*"?|;.*|[^\s\[\]{}('"`,;)]+)"###
+        )
+        .unwrap();
+    }
+    let mut res = vec![];
+    for cap in RE.captures_iter(str) {
+        if cap[1].starts_with(";") {
+            continue;
+        }
+        res.push(String::from(&cap[1]));
+    }
+    res
+}
+
+fn read_atom(rdr: &mut Reader) -> RlRet {
+    lazy_static! {
+        static ref INT_RE: Regex = Regex::new(r"^-?[0-9]+$").unwrap();
+        static ref STR_RE: Regex = Regex::new(r#""(?:\\.|[^\\"])*""#).unwrap();
+    }
+    let token = rdr.next()?;
+    match &token[..] {
+        "nil" => Ok(Nil),
+        "false" => Ok(Bool(false)),
+        "true" => Ok(Bool(true)),
+        _ => {
+            if INT_RE.is_match(&token) {
+                Ok(Int(token.parse().unwrap()))
+            } else {
+                Ok(Sym(token.to_string()))
+            }
+        }
+    }
+}
+
+fn read_form(rdr: &mut Reader) -> RlRet {
+    let token = rdr.peek()?;
+    match &token[..] {
+        "'" => {
+            let _ = rdr.next();
+            Ok(list![Sym("quote".to_string()), read_form(rdr)?])
+        }
+        _ => read_atom(rdr),
+    }
+}
+
+pub fn read_string(str: String) -> RlRet {
+    let tokens = tokenize(&str);
+    if tokens.len() == 0 {
+        return error("no input");
+    }
+    read_form(&mut Reader {
+        pos: 0,
+        tokens: tokens,
+    })
+}
